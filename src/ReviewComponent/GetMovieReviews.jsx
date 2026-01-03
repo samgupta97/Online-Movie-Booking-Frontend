a93dc68f9cef07d20ccd706b6dc04b8a54f25528
apoorv240701 <apoorvs2407@gmail.com>
2026-01-03 10:05:00 +0530

    apoorv240701: review UI and fetch integration

---
+++ src/ReviewComponent/GetMovieReviews.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import star from "../images/star.png";
+import BASE_URL from "../api/api";
+
+const GetMovieReviews = ({ movie }) => {
+  console.log("movie object");
+  console.log(JSON.stringify(movie));
+
+  const [reviews, setReviews] = useState([]);
+  const [rating, setRating] = useState("0.0");
+
+  const retrieveAllReviews = async () => {
+    const response = await axios.get(
+      `${BASE_URL}/movie/review/fetch?movieId=` + movie?.id
+    );
+    return response.data;
+  };
+
+  useEffect(() => {
+    const getAllReviews = async () => {
+      const allReviews = await retrieveAllReviews();
+
+      if (allReviews) {
+        setReviews(allReviews.reviews);
+        setRating(allReviews.averageRating);
+      }
+    };
+
+    getAllReviews();
+  }, []);
+
+  return (
+    <div
+      class="list-group form-card border-color"
+      style={{
+        height: "28rem",
+      }}
+    >
+      <div class="list-group-item list-group-item-action bg-color custom-bg-text">
+        <b>
+          Movie Rating: {rating}
+          <img
+            src={star}
+            width="20"
+            height="20"
+            className="d-inline-block align-top"
+            alt=""
+          />
+        </b>
+      </div>
+      <div
+        style={{
+          overflowY: "auto",
+        }}
+      >
+        {reviews.map((review) => {
+          return (
+            <div class="list-group-item list-group-item-action ">
+              <b className="">{review.user.firstName + " "}</b>
+              <b className="">{review.star + " /5 "}</b>
+              <img
+                src={star}
+                width="20"
+                height="20"
+                className="d-inline-block align-top"
+                alt=""
+              />
+              <br />
+              <p>{review.review}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default GetMovieReviews;
