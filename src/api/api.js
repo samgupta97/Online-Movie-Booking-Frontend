fed94b05bc8f913aa8a06e590b66d6fdd0b6a84a
samgupta97 <73067826+samgupta97@users.noreply.github.com>
2026-02-01 01:05:03 +0530

    changes in api.js faulty fixed

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,3 @@
-const BASE_URL = "online-movie-booking-backend-production.up.railway.app";
+const BASE_URL = "https://online-movie-booking-backend-production.up.railway.app";
 
 export default BASE_URL;
