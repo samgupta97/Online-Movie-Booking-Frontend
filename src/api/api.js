0af82f86b49c37cceb0e73ac0485a026ed8b01c4
samgupta97 <73067826+samgupta97@users.noreply.github.com>
2026-02-01 01:07:54 +0530

    changes in api.js added /api

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://online-movie-booking-backend-production.up.railway.app";
+const BASE_URL = "https://online-movie-booking-backend-production.up.railway.app/api";
 
 export default BASE_URL;
